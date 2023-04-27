2093a0a218b94f5c8880a2048e42c331e62787ab
thsilvadev <thsilva.developer@gmail.com>
2023-04-27 00:15:38 -0300

    I finished the DB Diagram and wrote the SQL, created the schema and executed the sql. DB is up. But it's far from final.  table shall have  column. And, still, I have to figure out a way of merging this DB model with the mtgJson one (the one that has all cards). And then, I have to learn knex and write the whole backend, test all routes on postman and depart to frontend. Much work to do!

---
+++ Project/MTG Card Vault.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `Cards` (
+  `id` int PRIMARY KEY NOT NULL,
+  `name` varchar(255),
+  `type` varchar(255),
+  `set` varchar(255),
+  `mana` int,
+  `image` varchar(255)
+);
+
+CREATE TABLE `Collection` (
+  `id_collection` int PRIMARY KEY NOT NULL,
+  `card_id` int,
+  `condition` varchar(255)
+);
+
+CREATE TABLE `Wishlist` (
+  `id_wishlist` int PRIMARY KEY NOT NULL,
+  `card_id` int,
+  `quantity` int,
+  `in_collection` int NOT NULL
+);
+
+CREATE TABLE `Decks` (
+  `id_deck` int PRIMARY KEY NOT NULL,
+  `name` varchar(255),
+  `color` varchar(255),
+  `description` varchar(255),
+  `card_count` int
+);
+
+CREATE TABLE `Deck` (
+  `id_constructed` int PRIMARY KEY NOT NULL,
+  `id_card` int,
+  `deck` int
+);
+
+ALTER TABLE `Collection` ADD FOREIGN KEY (`card_id`) REFERENCES `Cards` (`id`);
+
+ALTER TABLE `Wishlist` ADD FOREIGN KEY (`card_id`) REFERENCES `Cards` (`id`);
+
+ALTER TABLE `Deck` ADD FOREIGN KEY (`id_card`) REFERENCES `Collection` (`id_collection`);
+
+ALTER TABLE `Deck` ADD FOREIGN KEY (`deck`) REFERENCES `Decks` (`id_deck`);
