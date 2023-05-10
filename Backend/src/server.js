afdb2b26ac595aaa81a7c80742d463615292d34a
Thiago Pereira da Silva <thsilva.developer@gmail.com>
2023-05-10 20:02:29 -0300

    Backend seems to be working. I dit only getAll, getById and getBySet requests. Only for cards. I'm looking forward to build all CRUD for Cards,and then COPY method requests. At last, I want to refine the GETs for filtering, sorting and pagination (might have to learn 'join'). Off we go.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,8 +5,8 @@ const app = express();
 const routes = require('./routes');
 
 app.use(cors());
-app.use(express.json()); // Diz para o express que a resposta vai ser em JSON.
+app.use(express.json()); // Tells express that the response will be in JSON
 
-app.use(routes); // USA AS ROTAS FORNECIDADS PELO CONTROLER
+app.use(routes); // Use routes provided by controllers
 
 app.listen(3344, () => console.log('Servidor ON - Rodando na porta 3344 !!'));
