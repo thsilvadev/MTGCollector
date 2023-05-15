a70be324ff98ce97a7969866b356e85fda4aaac7
Thiago Pereira da Silva <thsilva.developer@gmail.com>
2023-05-15 04:30:49 -0300

    Today I messed with CSS a bit. Changed AboutUs picture border-radius and tried to implement some background-color change on scroll, but it still lacks Smooth. Something about the .active class that should be :active... it's conflicting with css modules. Gotta sleep, I'm calling it a win for a sunday to monday dawn

--- Frontend/src/layout/Container.js
+++ Frontend/src/layout/Container.js
@@ -1,13 +1,39 @@
 //styles
-import styles from '../styles/Container.module.css';
+
+    //Unfortunately here I´ll be using normal CSS and not CSS Modules because I need this .active thing to work well for now.
+import 
+//styles from 
+'../styles/Container.css';
 
 
 //routes
 import AppRoutes from "../routes/index";
 
+//hooks (for Background change on scroll)
+import React, {useState, useEffect} from 'react';
+
+
+
 const Container = () => {
+
+    //Background change on scroll
+    const [container, setContainer] = useState(false);
+
+    const changeBackground = () => {
+        console.log(window.scrollY)
+        if (window.scrollY >= 80) {
+            setContainer(true)
+        } else {
+            setContainer(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground)
+
+
+    //Return
     return (
-        <div className={styles.Container}>
+        <div className={container ? 'Container active' : 'Container'}>
             <AppRoutes />
         </div>
     )
--- Frontend/src/pages/AboutUs.js
+++ Frontend/src/pages/AboutUs.js
@@ -6,17 +6,18 @@ import styles from '../styles/AboutUs.module.css'
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1 className={styles.title} >About MTGCollector</h1>
             <div>
                 <img src={img1} className={styles.image1} alt="something"/>
             </div>
             <div className={styles.text}>
             <p>I'm thsilvadev but you can call me Thiagosauro, a casual <i>Magic: The Gathering</i> player. I used to play it 13 years ago back in the school days. Classic nerd twelve year olds group started to visit "Pergaminho" (word for Scroll, in english), a RPG store near our school. There we took part in dubious RPG sessions until the dungeon's keepers showed us Magic. It was unbeliavably magical. We all know it is.</p> <br/>
-            <p>Unfortunately at that time I had to choose between eating/going to the near LAN House and buying cards. So I pretty much stopped playing MTG until recently I bought 467 cards. With that amount, how many decks are possible? Soon I realized, though, I had few good cards I wish to use in more than one deck. But interchanging 'em like that would get a bit confused, I'd had to take notes and... why not make a digital collectior? That's just it! </p>
+            <p>Unfortunately at that time I had to choose between eating, going to the near LAN House and buying cards. So I pretty much stopped playing MTG (and eating) until recently I bought 467 cards, almost randomly. With that amount, how many decks are possible? Soon I realized, though, I had just a few good cards I wish to use in more than one deck. But interchanging 'em like that would get a bit confused, I'd had to take notes and... why not make a digital collectior? That's just it! </p>
             <p> And it's public! Enjoy! And if you have any doubts or feedback, please contact us at <a href="/contact">here</a>, it would be much appreciated!</p>
             </div>
         </div>
+        
     )
 }
 
