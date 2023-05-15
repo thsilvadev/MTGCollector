fa6800abaeaa7d29baf179de2e05bc184802ce12
Thiago Pereira da Silva <thsilva.developer@gmail.com>
2023-05-15 04:33:06 -0300

    Changed IP and database name (on laptop its mtg)

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,9 +4,15 @@ const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
+const port = 3344;
+const ip = '192.168.0.88';
+
 app.use(cors());
 app.use(express.json()); // Tells express that the response will be in JSON
 
 app.use(routes); // Use routes provided by controllers
 
-app.listen(3344, () => console.log('Servidor ON - Rodando na porta 3344 !!'));
+app.listen(port, ip, () => {
+
+    console.log(`Server is on - Running at http://${ip}:${port}/   !!`)
+})
