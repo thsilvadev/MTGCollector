b920394922734a97ee11572a094de72b81fb90ca
Thiago Pereira da Silva <thsilva.developer@gmail.com>
2023-05-22 04:00:39 -0300

    Filtering and Pagination done. Not stresstested them but ran into some bugs and already debugged. Next step is to test it all and update DB because images are missing.

--- Frontend/src/components/Card.js
+++ Frontend/src/components/Card.js
@@ -6,7 +6,7 @@ import styles from "../styles/Card.module.css"
 
 function Card ({ id, type, cardname, setCode, rarity, cost, condition, multiverseId }) {
     return (
-        <div className="col-12 col-sm-6 col-lg-4">
+        <div className="col-12 col-sm-6 col-lg-3">
             <img src={"https://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=" + multiverseId} alt="card" className={styles.CardContainer}/>
         </div>
     )
