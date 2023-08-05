481a8272c94b838696561a5e7e5c428bbd677aa5
thiagosauro <thsilva.developer@gmail.com>
2023-08-05 19:29:45 -0300

    Just changed Deck page name to DeckPage (because there is the Deck component, which is other thing)

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Collection from "../pages/Collection";
 import Decks from "../pages/Decks";
-import Deck from "../pages/Deck";
+import DeckPage from "../pages/DeckPage";
 import Wishlist from "../pages/Wishlist";
 import AboutUs from "../pages/AboutUs";
 import Contact from "../pages/Contact";
@@ -14,7 +14,7 @@ function AppRoutes() {
 
         <Route path="/collection" element={<Collection />} />
         <Route path="/decks" element={<Decks />} />
-        <Route path="/deck/:id" element={<Deck />} />
+        <Route path="/deck/:id" element={<DeckPage />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
