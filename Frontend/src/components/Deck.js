7527d82059f87140940227f8e3c51de64e16f018
thiagosauro <thsilva.developer@gmail.com>
2023-09-23 17:37:41 -0300

    clicking on Deck goes to /Collection and selects the deck to change
    also password nows has asteriscs

--- Frontend/src/components/Deck.js
+++ Frontend/src/components/Deck.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import Axios from "axios";
 import { useAuthHeader } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
 import styles from "../styles/Deck.module.css";
 
-const Deck = ({ colorIdentity, description, id_deck, name, toggler, cardCount }) => {
+const Deck = ({
+  colorIdentity,
+  description,
+  id_deck,
+  name,
+  toggler,
+  cardCount,
+}) => {
 
-
+  const navigate = useNavigate();
+  // Define the selected property (it can be null or a string)
   const [isHovering, setIsHovering] = useState(false);
 
   const handleMouseEnter = (e) => {
@@ -34,8 +43,8 @@ const Deck = ({ colorIdentity, description, id_deck, name, toggler, cardCount })
     },
   };
 
-
-  const deleteDeck = () => {
+  const deleteDeck = (e) => {
+    e.stopPropagation() //this is for not triggering parent element onClick event.
     if (window.confirm(`You're deleting this deck. Confirm?`)) {
       Axios.delete(`${window.name}/decks/${id_deck}`, config).then(() => {
         console.log(`requested to delete ${name} from collection`);
@@ -44,12 +53,18 @@ const Deck = ({ colorIdentity, description, id_deck, name, toggler, cardCount })
     }
   };
 
+  const handleClick = () => {
+
+    navigate(`/collection?selected=${id_deck}`);
+  };
+
   return (
     <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
       <div
         className={styles.deckContainer}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
       >
         <button className={hoveringClass} onClick={deleteDeck}>
           &times;
