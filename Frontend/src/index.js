40498b2e2593f8e2667e101e499492b0b1842df5
thiagosauro <thsilva.developer@gmail.com>
2023-09-25 01:55:05 -0300

    https

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -16,7 +16,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //global variable declaration workaround for API Address.
-window.name = "http://api.mtgchest.com";
+window.name = "https://api.mtgchest.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
