e1517d14b2e81ebd85ee0ddc1cf156ac5cf802db
Thiago Pereira da Silva <thsilva.developer@gmail.com>
2023-10-19 03:36:35 +0000

    Commiting backend which hasn't been commited in a long time (ever since online hosting).

--- Backend/src/controllers/decksController.js
+++ Backend/src/controllers/decksController.js
@@ -1,4 +1,5 @@
 const knex = require("../database/index");
+//authentication
 
 module.exports = {
   //This is a function to get all cards from Collection
@@ -11,6 +12,9 @@ module.exports = {
 
     const { page } = req.params;
 
+    //Authenticated userId
+    const user_id = req.userId;
+
     try {
       const result = await knex
 
@@ -18,14 +22,17 @@ module.exports = {
         "decks.name",
         "decks.description",
         "decks.color",
-        "decks.id_deck"
+        "decks.id_deck",
+        "decks.card_count"
+        
       )
       .from("decks")
+      .where("decks.user_id", user_id)
       .orderBy("decks.id_deck", "desc")
-      .limit(20)
-      .offset(page * 20);
+      .limit(15)
+      .offset(page * 15);
 
-    console.log(`Successfully got decks. Request by ${req.ip}`)
+    console.log(`Successfully got decks  of user${user_id}. Request by ${req.ip}`)
     return res.json(result);
 
 
@@ -50,17 +57,22 @@ module.exports = {
     const { name } = body;
     const { description } = body;
     const {color} = body;
+    const {card_count} = body;
+
+    //Authenticated userId
+    const user_id = req.userId
 
     try {
       const result = await knex("decks").insert({
         name,
        description,
        color,
-       card_count,
+       user_id,
+       card_count
       });
 
       console.log(
-        `Post successful of ${body} on Collection by ${req.ip} at ${formattedDate}`
+        `Post successful of ${body} on Collection of user${user_id} by ${req.ip} at ${formattedDate}`
       );
 
       return res.json(result);
@@ -73,6 +85,83 @@ module.exports = {
     }
   },
 
+  async deleteById(req, res) {
+    //Console logging with IP and Date (in yellow)
+    const now = new Date();
+    let formattedDate = `\x1b[33m${now.toISOString()}\x1b[0m`;
 
+    //Params request
+    const { id_deck } = req.params;
+
+    //Authenticated userId
+    const user_id = req.userId;
+
+    try {
+      const result = await knex
+        .select("id_deck")
+        .from("decks")
+        .where("decks.user_id" , user_id)
+        .where(`id_deck`, id_deck )
+        .del();
+
+      console.log(
+        `Delete successful of deck number "${id_deck}" of user ${user_id} by ${req.ip} at ${formattedDate}`
+      );
+
+      return res.json(result);
+    } catch (error) {
+      console.error(
+        `IP: ${req.ip}, Time: ${formattedDate}, id_deck: ${id_deck} ERROR:`,
+        error
+      );
+      return res.status(500).json({
+        error: "something went wrong",
+      });
+    }
+  },
+
+  //Update Deck
+  async updateDeck(req, res) {
+    //Console logging with IP and Date (in yellow)
+    const now = new Date();
+    let formattedDate = `\x1b[33m${now.toISOString()}\x1b[0m`;
+
+    //This is a body request. Data comes in json and not in params.
+    const body = req.body;
+    const {id_deck} = req.params
+    const {card_count} = body;
+    const { name } = body;
+    const { description } = body;
+    const {color} = body;
+
+    //Authenticated userId
+    const user_id = req.userId
+
+    try {
+      const result = await knex
+      .select("decks.id_deck")
+      .from("decks")
+      .where("decks.user_id", user_id)
+      .where("decks.id_deck", id_deck)
+      .update({
+        name,
+        color,
+       description,
+       card_count,
+      });
+
+      console.log(
+        `Put successful of ${color} and ${card_count} on Deck ${id_deck} of user ${user_id} by ${req.ip} at ${formattedDate}`
+      );
+
+      return res.json(result);
+    } catch (error) {
+      console.error(`IP: ${req.ip}, Time: ${formattedDate}. ERROR:`, error);
+      return res.status(500).json({
+        error:
+          "TO BE UPDATE // TO BE UPDATED  // TO BE UPDATED // TO BE UPDATED // TO BE UPDATED // TO BE UPDATED.",
+      });
+    }
+  },
 
 };
---
+++ Backend/src/email/email.js
@@ -0,0 +1,174 @@
+//Tools
+require(`dotenv`).config();
+
+const knex = require("../database/index");
+const nodeMailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+/*
+    
+
+  ______   ______   .__   __. .___________.    ___       ______ .___________.
+ /      | /  __  \  |  \ |  | |           |   /   \     /      ||           |
+|  ,----'|  |  |  | |   \|  | `---|  |----`  /  ^  \   |  ,----'`---|  |----`
+|  |     |  |  |  | |  . `  |     |  |      /  /_\  \  |  |         |  |     
+|  `----.|  `--'  | |  |\   |     |  |     /  _____  \ |  `----.    |  |     
+ \______| \______/  |__| \__|     |__|    /__/     \__\ \______|    |__|     
+                                                                             
+ 
+
+*/
+async function mainMail(name, email, subject, message) {
+    const transporter = await nodeMailer.createTransport({
+        host: "box.bestcontact.email",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "noreply@mtgchest.com",
+            pass: "F6BOsXlwGvrPjWUa025ckR",
+        }
+    });
+
+    //Some sanitization is necessary, as the JSON Literal comes with \n for newlines and we need to translate it to HTML <br> tag. 
+    const htmledMessage = message.replace(/\n/g, '<br>');
+
+    //Also did some <b/> tags and <br> tags to better format our email bodies from Contact Form.
+    const mailOption = {
+        from: "noreply@mtgchest.com",
+        to: "contact@mtgchest.com",
+        subject: `${subject} // ${email}`, /* I want it to display the subject and the email right in the inbox */
+        html: `You got a message from<br><b>Email</b> : ${email}<br><b>Name:</b> ${name}<br><b>Subject:</b> ${subject}<br><b>Message:</b><br> ${htmledMessage}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOption);
+        return Promise.resolve("Message Sent Successfully!");
+      } catch (error) {
+        return Promise.reject(error);
+      }
+}
+
+/*
+
+ _______ .___  ___.      ___       __   __                                                                            
+|   ____||   \/   |     /   \     |  | |  |                                                                           
+|  |__   |  \  /  |    /  ^  \    |  | |  |                                                                           
+|   __|  |  |\/|  |   /  /_\  \   |  | |  |                                                                           
+|  |____ |  |  |  |  /  _____  \  |  | |  `----.                                                                      
+|_______||__|  |__| /__/     \__\ |__| |_______|                                                                      
+                                                                                                                      
+  ______   ______   .__   __.  _______  __  .______      .___  ___.      ___   .___________. __    ______   .__   __. 
+ /      | /  __  \  |  \ |  | |   ____||  | |   _  \     |   \/   |     /   \  |           ||  |  /  __  \  |  \ |  | 
+|  ,----'|  |  |  | |   \|  | |  |__   |  | |  |_)  |    |  \  /  |    /  ^  \ `---|  |----`|  | |  |  |  | |   \|  | 
+|  |     |  |  |  | |  . `  | |   __|  |  | |      /     |  |\/|  |   /  /_\  \    |  |     |  | |  |  |  | |  . `  | 
+|  `----.|  `--'  | |  |\   | |  |     |  | |  |\  \----.|  |  |  |  /  _____  \   |  |     |  | |  `--'  | |  |\   | 
+ \______| \______/  |__| \__| |__|     |__| | _| `._____||__|  |__| /__/     \__\  |__|     |__|  \______/  |__| \__| 
+                                                                                                                      
+
+*/
+
+async function configMail (email) {
+    const transporter = await nodeMailer.createTransport({
+        host: "box.bestcontact.email",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "noreply@mtgchest.com",
+            pass: "F6BOsXlwGvrPjWUa025ckR",
+        }
+    });
+
+    const emailToken = jwt.sign({email: email}, 'totaldeathmetal')
+
+    const url = `http://mtgchest.com/confirmation/${emailToken}`
+
+    //Also did some <b/> tags and <br> tags to better format our email bodies from Contact Form.
+    const mailOption = {
+        from: "noreply@mtgchest.com",
+        to: email,
+        subject:`MTG Chest: Confirmation Email`, /* I want it to display the subject and the email right in the inbox */
+        html: `Please, click here to <strong>confirm your email</strong> and finish sign in:<br><br><a href="${url}">${url}</a>`,
+    };
+
+    try {
+      await transporter.sendMail(mailOption);
+      return Promise.resolve("Message Sent Successfully!");
+    } catch (error) {
+      return Promise.reject(error);
+    }
+}
+
+module.exports = {
+
+  //As user submits a contact form, send it to noreply@mtgchest.com
+    async contactForm (req, res) {
+        const { yourname, youremail, yoursubject, yourmessage } = req.body;
+        try {
+          await mainMail(yourname, youremail, yoursubject, yourmessage);
+          
+          res.send("Message Successfully Sent!");
+        } catch (error) {
+          res.send("Message Could not be Sent");
+        }
+      }
+
+    ,
+  //As user registers or try to login in a not confirmed account > send email to confirm.
+    async confirmEmail (req) {
+      const {email} = req.body;
+      try {
+        await configMail(email);
+
+        return "Confirmation email successfully sent!";
+        } catch (error) {
+          throw new Error("Confirmation email could not be Sent");
+        }
+    }
+
+    ,
+
+  //check if user clicked on the link sent on email with corresponding emailToken (jwt)
+    async checkUserConfirm (req, res) {
+
+
+      const {emailToken} = req.params;
+      const {confirmed} = req.body;
+      console.log(confirmed);
+      try {
+      
+      const data = jwt.verify(emailToken, "totaldeathmetal");
+      if (!data.email){
+        return res.sendStatus(401)
+      } else {
+       await knex('users')
+        .where('email', data.email)
+        .update({
+          confirmed: confirmed
+        });
+        console.log(`User successfully confirmed email ${data.email}. Registration complete.`);
+        res.status(200).json({message: "You have successfully confirmed your email. You may proceed to login."})
+      }
+
+
+
+    } catch (error) {
+      console.error("JWT verification error:", error);
+      return res.status(401).json({error: 'Sorry. Something went wrong :/'});
+    }
+    }
+}
+
+
+
+
+/* 
+  setting service: 'gmail' is same as providing the settings manually in the transport object above.
+
+{
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true
+}
+
+https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
+*/
+
